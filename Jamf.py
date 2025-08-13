0575e17744b8a544610534a21124b2a1cefe20d2
Vivek Kumar <vivekvaid1@icloud.com>
2025-08-13 19:38:17 +0530

    Create Jamf.py

---
+++ Jamf.py
@@ -0,0 +1,15 @@
+# save this file as app.py
+import streamlit as st
+
+# Set the page title
+st.set_page_config(page_title="Jamf App Status", page_icon="🚧")
+
+# Display the heading
+st.title("🚧 Jamf App is under construction 🚧")
+
+# Optional: Add extra text or styling
+st.markdown(
+    """
+    ---
+    We are working hard to improve your experience.  
+    Please check back soon
